c06d832fca08abe094a86109081e79b7ac3e6c74
Silver <iasmin.brebu@student.upt.ro>
2024-05-26 20:51:17 +0300

    You can now see all products! Yay! Am tired

--- src/main/java/cs/upt/store/controller/ProductController.java
+++ src/main/java/cs/upt/store/controller/ProductController.java
@@ -1,6 +1,7 @@
 package cs.upt.store.controller;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 import javax.naming.NameNotFoundException;
 
@@ -11,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -68,6 +70,16 @@ public class ProductController {
             return new ResponseEntity<>(new ProductDTO("No product to delte has been found", false), HttpStatus.NOT_FOUND);
         }catch(Exception e){
             return new ResponseEntity<>(new ProductDTO("Server-side error", false), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductDTO>> getProducts(){
+        try{
+            List<ProductDTO> products = productService.getProducts();
+            return new ResponseEntity<>(products, HttpStatus.OK);
+        }catch(Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 } 
--- src/main/java/cs/upt/store/service/ProductService.java
+++ src/main/java/cs/upt/store/service/ProductService.java
@@ -1,6 +1,7 @@
 package cs.upt.store.service;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.naming.NameNotFoundException;
@@ -11,6 +12,7 @@ import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Service;
 
 import cs.upt.store.DTO.HashedUserDTO;
+import cs.upt.store.DTO.ProductDTO;
 import cs.upt.store.exceptions.UserIsNotASellerException;
 import cs.upt.store.model.Product;
 import cs.upt.store.repository.ProductRepository;
@@ -77,5 +79,14 @@ public class ProductService {
         }
         productRepository.deleteById(found.getPid());
         return found;
+    }
+
+    public List<ProductDTO> getProducts(){
+        List<Product> all = productRepository.findAll();
+        List<ProductDTO> result = new ArrayList<ProductDTO>();
+        for (int i = 0; i < all.size(); i++) {
+            result.add(new ProductDTO(all.get(i), true, null));
+        }
+        return result;
     }
 }
