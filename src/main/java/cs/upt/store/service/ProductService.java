da87f522127252e7cc6ac4c2d60be5f70628ebf3
Silver <iasmin.brebu@student.upt.ro>
2024-05-26 18:50:59 +0300

    You can now add at /v1/products, could not auto verify if seller is part of the document with @ReferencesDocument, but I fixed in in ProductService

---
+++ src/main/java/cs/upt/store/service/ProductService.java
@@ -0,0 +1,40 @@
+package cs.upt.store.service;
+
+import java.security.NoSuchAlgorithmException;
+
+import javax.naming.NameNotFoundException;
+
+import org.apache.coyote.BadRequestException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cs.upt.store.DTO.HashedUserDTO;
+import cs.upt.store.exceptions.UserIsNotASellerException;
+import cs.upt.store.model.HashedUser;
+import cs.upt.store.model.Product;
+import cs.upt.store.repository.ProductRepository;
+
+
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private UserService userService;
+
+    public Product insertProduct(Product product) throws NoSuchAlgorithmException, NameNotFoundException, UserIsNotASellerException{
+        try{
+            HashedUserDTO seller = userService.getUser(product.getSeller());
+            if(seller.getType() == 0){
+                throw new UserIsNotASellerException("Not a seller");
+            }
+            return productRepository.insert(product);
+        }catch(NameNotFoundException e){
+            throw e;
+        }catch(Exception e){
+            throw e;
+        }
+    }
+}
