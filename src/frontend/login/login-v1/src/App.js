5479f25841d2864239d8220aa907bdd39d66deda
Alexandru <Burada Alexandru>
2024-05-27 17:13:29 +0300

    create home botton

--- src/frontend/login/login-v1/src/App.js
+++ src/frontend/login/login-v1/src/App.js
@@ -1,47 +1,39 @@
-import '.App.css';
+import './App.css';
 import api from './api/axiosConfig';
 import { useState, useEffect } from 'react';
-import layout from './components/Layout'; 
+import Layout from './components/Layout'; 
 import {Routes, Route} from 'react-router-dom';
+import Home from './components/home';
 
-function ProductList() {
-  const [products, setProducts] = useState([]);
+function App() {
+  const [products, setProducts] = useState();
 
-  useEffect(() => {
-    async function fetchProducts() {
-      try {
-        const response = await axios.get('/api/products'); // Aici trebuie să fie adresa corectă a endpoint-ului care returnează produsele
-        setProducts(response.data);
-      } catch (error) {
-        console.error('Error fetching products:', error);
-      }
+  const getProducts = async () => {
+  
+    try {
+      const response = await axios.get('/api/products'); // Aici trebuie să fie adresa corectă a endpoint-ului care returnează produsele
+      
+      setProducts(response.data);
+
+    } catch (error) {
+      console.error('Error fetching products:', error);
     }
 
-    fetchProducts();
-  }, []);
+  } 
+
+  useEffect(() => {
+    getProducts();
+  }, [])
 
   return (
-    <div> 
+    <div className = "App">
       <Routes> 
-        <Route path="/" element={<Layout />} />
-          
+        <Route path="/" element={<Layout />} > </Route>
+        <Route path="/" element={<Home />} > </Route>
       </Routes>
 
-      <h1>Product List</h1>
-      <ul>
-        {products.map((product, index) => (
-          <li key={index}>
-            <h2>{product.name}</h2>
-            <img src={product.imgs} alt={product.name} />
-            <p>Seller: {product.seller}</p>
-            <p>Price: {product.price}</p>
-            <p>Status: {product.status ? 'Available' : 'Out of stock'}</p>
-            <p>Message: {product.message}</p>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
 
-export default ProductList;
+export default App;
